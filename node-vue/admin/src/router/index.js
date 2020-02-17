e7113d6fb27972770bb5a7558fddc348e092e122
yiqiwang <yiqiwang@tencent.com>
2020-02-17 20:01:57 +0800

    完成分类页面

--- node-vue/admin/src/router/index.js
+++ node-vue/admin/src/router/index.js
@@ -8,16 +8,19 @@ const routes = [
   {
     path: '/',
     name: 'Main',
-    component: Main
-  }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+    component: Main,
+    children: [
+      {
+        path: '/categories/create',
+        name: 'CategoryEdit',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('../views/CategoryEdit.vue')
+      }
+    ]
+  },
+
 ]
 
 const router = new VueRouter({
---
+++ node-vue/admin/src/views/CategoryEdit.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>创建分类</h1>
+        <el-form>
+            <!--     原生表单阻止默认提交跳转页面       -->
+            <el-form-item label="名称" label-width="120px" @submit.native.prevent="save">
+                <el-input v-model="model.name"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" native-type="submit">保存</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "CategoryEdit",
+    data () {
+      return {
+        model: {}
+      }
+    },
+    methods: {
+      save () {
+        // this.$http.post()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
