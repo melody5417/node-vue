45e43aa0f0009b525767af2258b0cc2d44a1523a
yiqiwang <yiqiwang@tencent.com>
2020-02-17 12:06:04 +0800

    安装stylus loader， 完成admin主界面初始化

---
+++ node-vue/admin/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'stylus',
+      patterns: []
+      // https://blog.csdn.net/weixin_41786576/article/details/83745702
+      // 如果你想自动化导入文件 (用于颜色、变量、mixin……)，你可以使用 style-resources-loader
+    }
+  }
+}
