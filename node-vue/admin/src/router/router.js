3322bdfc9e61fd02f5a59cf06500cb138efa0708
yiqiwang <yiqiwang@tencent.com>
2020-02-23 22:45:23 +0800

    添加物品管理

--- node-vue/admin/src/router/router.js
+++ node-vue/admin/src/router/router.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Main from '../views/Main'
 import CategoryEdit from "../views/CategoryEdit"
 import CategoryList from "../views/CategoryList"
+import ItemEdit from "../views/ItemEdit"
+import ItemList from "../views/ItemList"
 
 Vue.use(VueRouter)
 
@@ -25,6 +27,19 @@ const routes = [
         path: '/categories/list',
         name: 'CategoryList',
         component: CategoryList
+      }, {
+        path: '/items/create',
+        name: 'ItemCreate',
+        component: ItemEdit
+      }, {
+        path: '/items/edit/:id',
+        name: 'ItemEdit',
+        component: ItemEdit,
+        props: true     // 两个页面对应一个组件 加上props：true，表示把所有参数都注入到这个页面
+      }, {
+        path: '/items/list',
+        name: 'ItemList',
+        component: ItemList
       }
     ]
   },
---
+++ node-vue/admin/src/views/ItemEdit.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <h1>{{ id ? '编辑' : '新建'}}物品</h1>
+        <!--     原生表单阻止默认提交跳转页面       -->
+        <el-form label-width="120px" @submit.native.prevent="save">
+            <el-form-item label="名称">
+                <el-input v-model="model.name"></el-input>
+            </el-form-item>
+            <el-form-item label="图标">
+                <el-input v-model="model.icon"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" native-type="submit">保存</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "ItemEdit",
+    props: {
+      id: {}    // 这样写尽可能的和路由解耦合 相比 this.$route.params.id
+    },
+    data () {
+      return {
+        model: {}
+      }
+    },
+    methods: {
+      async save () {
+        console.log('save')
+        let res
+        if (this.id) {
+          res = await this.$http.put('/rest/items/' + this.id, this.model)
+        } else {
+          res = await this.$http.post('/rest/items', this.model)
+        }
+        console.log(res)
+        this.$router.push('/items/list')
+        this.$message({
+          type: 'success',
+          message: '保存成功'
+        })
+      },
+      async fetch () {
+        const res = await this.$http.get('/rest/items/' + this.id)
+        this.model = res.data
+      }
+    },
+    created() {
+      this.id && this.fetch()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
