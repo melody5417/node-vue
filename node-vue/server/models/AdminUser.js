59f1631ef8d8a3bf0f70f07cb7de10ac5a1513ee
yiqiwang <yiqiwang@tencent.com>
2020-04-01 11:04:16 +0800

    密码加密

--- node-vue/server/models/AdminUser.js
+++ node-vue/server/models/AdminUser.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
   username: { type: String },
-  password: { type: String }
+  password: {
+    type: String,
+    select: false,  // 设置无法查询 密码一般都是设置 无法查询
+    set(val) {
+      // 加密后密码
+      // npm i bcrypt --save 此处如果bcrypt安装失败 可以安装bcryptjs
+      return require('bcrypt').hashSync(val, 10)
+    }
+  }
 })
 
 module.exports = mongoose.model('AdminUser', schema)
