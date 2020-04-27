cf004ba11c1c792a2418c7bea8ebbf0485eefa75
yiqiwang(王一棋) <yiqiwang@tencent.com>
2020-04-27 09:59:44 +0800

    修复 DeprecationWarning

--- node-vue/server/plugins/db.js
+++ node-vue/server/plugins/db.js
@@ -5,6 +5,7 @@
 module.exports = app => {
   const mongoose = require("mongoose")
   mongoose.connect('mongodb://127.0.0.1/node-vue-moba', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
 }
