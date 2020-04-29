6dbe06431801d6a180042d5932a9789609247182
yiqiwang <yiqiwang@tencent.com>
2020-04-29 22:52:27 +0800

    添加 mongodb authentication

--- node-vue/server/plugins/db.js
+++ node-vue/server/plugins/db.js
@@ -4,7 +4,19 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = app => {
   const mongoose = require("mongoose")
-  mongoose.connect('mongodb://127.0.0.1/node-vue-moba', {
+  // 添加用户认证
+  // /usr/local/Cellar/mongodb/4.0.3_1/bin 下 运行 ./mongo
+  // > show dbs
+  // admin          0.000GB
+  // config         0.000GB
+  // local          0.000GB
+  // node-vue-moba  0.000GB
+  // > use node-vue-moba
+  // switched to db node-vue-moba
+  // > db.createUser({user:'admin', pwd:'password', roles:['readWrite']})
+  // Successfully added user: { "user" : "admin", "roles" : [ "readWrite" ] }
+
+  mongoose.connect('mongodb://admin:password@127.0.0.1/node-vue-moba', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
